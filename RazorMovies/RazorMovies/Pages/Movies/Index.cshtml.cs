b49f5676ce6427a7d3547b17e8691a4f4f4be45f
Jorge Martinez Rodriguez <jorgeemtnz@gmail.com>
2025-04-08 20:33:22 -0300

    added filters

--- RazorMovies/RazorMovies/Pages/Movies/Index.cshtml.cs
+++ RazorMovies/RazorMovies/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorMovies.Data;
 using RazorMovies.Models;
@@ -19,11 +20,31 @@ namespace RazorMovies.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; } = default!;
+        public IList<Movie> Movie { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+        public SelectList? Genres { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(SearchString));
+            }
+
+            if(!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await _context.Movie.OrderBy(x => x.Genre).Select(x => x.Genre).Distinct().ToListAsync());
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
