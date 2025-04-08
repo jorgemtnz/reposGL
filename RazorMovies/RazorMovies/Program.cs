31bc7152992c8679e7a38ad2ac1ab632fa2a47f9
Jorge Martinez Rodriguez <jorgeemtnz@gmail.com>
2025-04-08 18:18:06 -0300

    added new pages and migration

--- RazorMovies/RazorMovies/Program.cs
+++ RazorMovies/RazorMovies/Program.cs
@@ -1,10 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorMovies.Data;
+using RazorMovies.Models;
+using RazorMovies.SeedData;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorMoviesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorMoviesContext") ?? throw new InvalidOperationException("Connection string 'RazorMoviesContext' not found.")));
 
 var app = builder.Build();
 
+using(var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        SeedData.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
---
+++ RazorMovies/RazorMovies/SeedData/SeedData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorMovies.Data;
+using RazorMovies.Models;
+
+namespace RazorMovies.SeedData
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorMovies.Data.RazorMoviesContext(
+                serviceProvider.GetRequiredService<DbContextOptions<RazorMoviesContext>>()))
+            {
+                if( context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorMoviesContext");
+                }
+
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Shawshank Redemption",
+                        ReleaseDate = DateTime.Parse("1994-9-23"),
+                        Genre = "Drama",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "The Godfather",
+                        ReleaseDate = DateTime.Parse("1972-3-24"),
+                        Genre = "Crime",
+                        Price = 12.99M
+                    },
+                    new Movie
+                    {
+                        Title = "The Dark Knight",
+                        ReleaseDate = DateTime.Parse("2008-7-18"),
+                        Genre = "Action",
+                        Price = 14.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
